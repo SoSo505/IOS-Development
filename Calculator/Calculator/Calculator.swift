71b8b3a4e2e82f819694220f08c33be77fb21402
SoSo505 <53016398+SoSo505@users.noreply.github.com>
2020-02-01 15:50:08 +0600

    Add files via upload

---
+++ Calculator/Calculator/Calculator.swift
@@ -0,0 +1,83 @@
+//
+//  Calculator.swift
+//  CalculatorProject
+//
+//  Created by erumaru on 1/25/20.
+//  Copyright © 2020 kbtu. All rights reserved.
+//
+import Foundation
+
+class Calculator {
+    // MARK: - Constants
+    enum Operation {
+        case equals
+        case constant(value: Double)
+        case unary(function: (Double) -> Double)
+        case binary(function: (Double, Double) -> Double)
+    }
+    
+    var map: [String : Operation] = [
+        "+" : .binary { $0 + $1 },
+        "-" : .binary { $0 - $1 },
+        "x" : .binary { $0 * $1},
+        "%" : .binary { (($0 * $1)/100)},
+        "=" : .equals,
+        "/" : .binary {$0 / $1},
+        "pi": .constant (value: Double.pi),
+        "sqrt": .unary { a in return sqrt(a) },
+        "x^y" : .binary {x,y in pow(x,y)},
+        "x!" : .unary { n in
+            if n == 0 {
+                return 1
+            }
+            var a: Double = 1
+            for i in 1...Int(n) {
+                a *= Double(i)
+            }
+            return a
+        },
+        "AC" : .constant(value:0),
+        "Rand" : .constant(value: Double.random(in: 1...100)),
+    
+    ]
+        
+    // MARK: - Variables
+    var result: Double = 0
+    var lastBinaryOperation: ((Double, Double) -> Double)?
+    var reminder: Double = 0
+    
+    // MARK: - Methods
+            
+    func setOperand(number: Double) {
+        result = number
+    }
+    
+    func executeOperation(symbol: String) {
+        guard let operation = map[symbol] else {
+            print("ERROR: no such symbol in map")
+            return
+        }
+        
+        switch operation {
+        case .binary(let function):
+        if lastBinaryOperation != nil {
+            executeOperation(symbol: "=")
+        }
+        
+        lastBinaryOperation = function
+        reminder = result
+        case .unary(let function):
+            result = function(result)
+        
+        case .constant(let value):
+            result = value
+                
+        case .equals:
+            if let lastOperation = lastBinaryOperation {
+                result = lastOperation(reminder, result)
+                lastBinaryOperation = nil
+                reminder = 0
+            }
+        }
+    }
+}
---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  CalculatorProject
+//
+//  Created by erumaru on 1/25/20.
+//  Copyright © 2020 kbtu. All rights reserved.
+//
+import UIKit
+
+class ViewController: UIViewController {
+
+    let model = Calculator()
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+     @IBOutlet weak var viewDidcontroller: UILabel!
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        guard
+            let numberText = sender.titleLabel?.text
+        else { return }
+        if(resultLabel?.text == "0.0" || resultLabel?.text == "0"
+            ){
+            resultLabel?.text = numberText;
+        }
+        else{ resultLabel.text?.append(numberText)
+        }
+    }
+    
+    @IBAction func operationPressed(_ sender: UIButton) {
+        guard
+            let numberText = resultLabel.text,
+            let number = Double(numberText),
+            let operation = sender.titleLabel?.text
+        else {
+            return
+        }
+        
+        resultLabel.text = "0.0"
+        model.setOperand(number: number)
+        model.executeOperation(symbol: operation)
+        
+        if operation == "=" {
+            resultLabel.text = "\(model.result)"
+        }
+        
+        if operation == "x!" {
+            resultLabel.text = "\(model.result)"
+        }
+        
+        if operation == "pi" {
+            resultLabel.text = "\(model.result)"
+        }
+        
+        if operation == "sqrt" {
+                   resultLabel.text = "\(model.result)"
+        }
+        
+        if operation == "Rand" {
+                   resultLabel.text = "\(model.result)"
+        }
+        if operation == "AC" {
+                          resultLabel.text = "\(model.result)"
+               }
+    }
+}
